3cba2b5248721d31ba92f942da99000d62eea193
Sina Sabet <sinasabet81@gmail.com>
2019-02-14 05:04:41 -0800

    fixing source paths in common/index.sh

--- .shell/common/index.sh
+++ .shell/common/index.sh
@@ -1,6 +1,6 @@
-source $HOME/.shell/exports.sh
-source $HOME/.shell/path.sh
-source $HOME/.shell/aliases.sh
+source $HOME/.shell/common/exports.sh
+source $HOME/.shell/common/path.sh
+source $HOME/.shell/common/aliases.sh
 
 # fzf stuff
 [ -f $HOME/.fzf.zsh ] && source $HOME/.fzf.zsh
