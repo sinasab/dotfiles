e5212cda2241a81629d63d064f221e7412b8801f
Sina Sabet <sinasabet81@gmail.com>
2019-03-07 13:35:52 -0800

    adding support for common local file

--- .shell/common/index.sh
+++ .shell/common/index.sh
@@ -1,3 +1,5 @@
 source $HOME/.shell/common/exports.sh
 source $HOME/.shell/common/path.sh
 source $HOME/.shell/common/aliases.sh
+FILE="$HOME/.shell/common/local.sh" && test -f $FILE && source $FILE
+unset FILE
